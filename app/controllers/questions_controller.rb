c73aa7aa305b61101e191591f1ceb55dd893bd64
Remy Cassagne <remy.engassac@icloud.com>
2024-11-11 12:15:25 +0100

    'done'

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,7 +9,7 @@ class QuestionsController < ApplicationController
     elsif @question.end_with?("?")
       @answer = "Silly question, get dressed and go to work!"
     else
-      @answer = "I don't care, get dressed and go to work! toto"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
